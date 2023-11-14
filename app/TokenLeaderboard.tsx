437ed1d884edfbcd69530d44c373e630a36fbd23
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2023-11-14 16:25:45 +0000

    Move fetchTokenInfo
    Move fetchTokenInfo into useEffect as an immediately invoked anonymous async function

--- app/TokenLeaderboard.tsx
+++ app/TokenLeaderboard.tsx
@@ -21,65 +21,51 @@ import {
 const TokenLeaderboard: FC<{ programId: string }> = (props) => {
   const connection = new Connection(GRAPE_RPC_ENDPOINT);
   const token = new PublicKey(props.programId);
-
-  // Define state variables for token information, holders, and loading status
+  
   const [tokenInfo, setTokenInfo] = useState<any | null>(null);
   const [holders, setHolders] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
-  const fetchTokenInfo = async() => {
-    try {
-    let tokenDetails = await connection.getParsedAccountInfo(token);
-    if (tokenDetails){
-      console.log("tokenDetails", tokenDetails)
-      setTokenInfo(tokenDetails.value.data.parsed.info);
-    }
-
-    const tokenMint = await getMint(connection, token);
-    console.log("tokenMint: ",tokenMint);
-
-    const tokenAccounts = await connection.getProgramAccounts(
-      //token program address
-      TOKEN_PROGRAM_ID,
-
-      {
-        filters: [
+  useEffect(() => { 
+    (async() => {
+      try {
+        let tokenDetails = await connection.getParsedAccountInfo(token);
+        if (tokenDetails){
+          setTokenInfo(tokenDetails.value.data.parsed.info);
+        }
+        const tokenMint = await getMint(connection, token);
+        const tokenAccounts = await connection.getProgramAccounts(
+          //token program address
+          TOKEN_PROGRAM_ID,
+  
           {
-            dataSize: 165, // Adjust the dataSize based on the actual size of your token account data
-          },
-          {
-            memcmp: {
-              offset: 0, // Adjust the offset based on the actual offset of the mint field in the account data
-              bytes: token,
-            },
-          },
-        ],
+            filters: [
+              {
+                dataSize: 165, // Adjust the dataSize based on the actual size of your token account data
+              },
+              {
+                memcmp: {
+                  offset: 0, // Adjust the offset based on the actual offset of the mint field in the account data
+                  bytes: token,
+                },
+              },
+            ],
+          }
+        )
+        const holders = await Promise.all(tokenAccounts.map(async (account) => {
+          const address = account.pubkey.toBase58()
+          const accountInfo = await getAccount(connection, account.pubkey)
+          return {
+            address: address,
+            balance: Number(accountInfo.amount)
+          }
+        }))
+        const sortedHolders = holders.sort((a, b) => b.balance - a.balance)  
+        setHolders(sortedHolders)
+      } catch(err) {
+        console.error("Error fetching token info:", err)
       }
-    )
-    console.log("tokenAccounts:", tokenAccounts)
-
-    const holders = await Promise.all(tokenAccounts.map(async (account) => {
-      const address = account.pubkey.toBase58()
-      const accountInfo = await getAccount(connection, account.pubkey)
-      return {
-        address: address,
-        balance: Number(accountInfo.amount)
-      }
-    }))
-    
-    const sortedHolders = holders.sort((a, b) => b.balance - a.balance)
-
-    console.log("sortedHolders:", sortedHolders)
-
-    setHolders(sortedHolders)
-    } catch(err) {
-      console.error("Error fetching token info:", err)
-    }
-  }
-
-  useEffect(() => {   
-    //if (true)
-      fetchTokenInfo();
+    })();
   }, []);
 
   return (

ed61ceb8eba2a1698fbc0af1d035fdca1a94ea16
Joshua Barratt <30458569+jbarratt77@users.noreply.github.com>
2023-11-14 16:29:07 +0000

    :memo: comment code

--- app/TokenLeaderboard.tsx
+++ app/TokenLeaderboard.tsx
@@ -1,4 +1,4 @@
-'use client'
+// Import necessary modules and components from libraries
 import { FC, useEffect, useState } from 'react';
 
 import { PublicKey, Connection, TokenAccountsFilter, LAMPORTS_PER_SOL } from '@solana/web3.js'; 
@@ -18,26 +18,33 @@ import {
   Typography,
 } from '@mui/material'
 
+// Define a React functional component named TokenLeaderboard
 const TokenLeaderboard: FC<{ programId: string }> = (props) => {
+  // Create a connection to the Solana blockchain using the specified RPC endpoint
   const connection = new Connection(GRAPE_RPC_ENDPOINT);
+  // Define the token's public key based on the provided programId prop
   const token = new PublicKey(props.programId);
   
+  // Define state variables for token information, holders, and loading status
   const [tokenInfo, setTokenInfo] = useState<any | null>(null);
   const [holders, setHolders] = useState<any[]>([]);
   const [loading, setLoading] = useState<boolean>(true);
 
+  // Use the useEffect hook to fetch token information and holders when the component mounts
   useEffect(() => { 
-    (async() => {
+    // Define an asynchronous function to handle token data fetching
+    (async () => {
       try {
+        // Fetch parsed account information for the specified token
         let tokenDetails = await connection.getParsedAccountInfo(token);
-        if (tokenDetails){
+        // If tokenDetails is available, update the state with token information
+        if (tokenDetails) {
           setTokenInfo(tokenDetails.value.data.parsed.info);
         }
-        const tokenMint = await getMint(connection, token);
+
+        // Fetch token accounts based on the token program ID and token's public key
         const tokenAccounts = await connection.getProgramAccounts(
-          //token program address
           TOKEN_PROGRAM_ID,
-  
           {
             filters: [
               {
@@ -52,26 +59,37 @@ const TokenLeaderboard: FC<{ programId: string }> = (props) => {
             ],
           }
         )
+
+        // Fetch account information for each token account in parallel
         const holders = await Promise.all(tokenAccounts.map(async (account) => {
-          const address = account.pubkey.toBase58()
-          const accountInfo = await getAccount(connection, account.pubkey)
+          const address = account.pubkey.toBase58();
+          const accountInfo = await getAccount(connection, account.pubkey);
           return {
             address: address,
             balance: Number(accountInfo.amount)
-          }
-        }))
+          };
+        }));
+
+        // Sort the holders array based on the balance in descending order
         const sortedHolders = holders.sort((a, b) => b.balance - a.balance)  
-        setHolders(sortedHolders)
+        // Update the state with the sorted holders array
+        setHolders(sortedHolders);
       } catch(err) {
-        console.error("Error fetching token info:", err)
+        // Log an error message if there is an error fetching token information
+        console.error("Error fetching token info:", err);
+      } finally {
+        // Set loading to false after fetching token information and holders
+        setLoading(false);
       }
     })();
-  }, []);
+  }, []); // Empty dependencies array ensures the effect runs only once when the component mounts
 
+  // Render the component with token information and holders
   return (
     <Box sx={{ flexGrow: 1 }}>
       <Divider>Token</Divider>
 
+      {/* Display token information if available */}
       <Typography>
         {tokenInfo &&
           <>
@@ -84,14 +102,23 @@ const TokenLeaderboard: FC<{ programId: string }> = (props) => {
 
       <Divider>Holders</Divider>
       <List>
-        {holders && holders.map((item, key) => (
-          <ListItem key={key}>
-            <Typography>Address: {item.address} Balance: {item.balance}</Typography>
-          </ListItem>
-        ))}
+        {/* Display loading message while fetching data, otherwise, display the list of holders */}
+        {loading ? (
+          <Typography>Loading...</Typography>
+        ) : (
+          holders.map((item, key) => (
+            // Display each holder's address and balance as a list item
+            <ListItem key={key}>
+              <Typography>
+                Address: {item.address} Balance: {item.balance}
+              </Typography>
+            </ListItem>
+          ))
+        )}
       </List>
     </Box>
   );
 }
 
+// Export the TokenLeaderboard component as the default export of this module
 export default TokenLeaderboard;

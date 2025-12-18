aec5b77379e3eec19b0f3b512b31c1ba801c1dfe
Kirk <kfournia@gmail.com>
2025-12-19 00:04:13 +0200

    added SSR support

--- app/components/DaoApp.tsx
+++ app/components/DaoApp.tsx
@@ -1,13 +1,24 @@
 "use client";
 
-import Providers from "./Providers";
-//import HomeInner from "./HomeInner"; // your current HomeInner extracted to its own file
+import React, { useMemo } from "react";
+import { clusterApiUrl } from "@solana/web3.js";
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+
+import HomeInner from "./HomeInner";
 
 export default function DaoApp() {
+  const endpoint = useMemo(() => clusterApiUrl("devnet"), []);
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
   return (
-    <Providers>
-      <></>
-      {/* <HomeInner /> */}
-    </Providers>
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <HomeInner />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
   );
 }
--- app/dao/[dao]/page_ssr.tsx
+++
@@ -1,6 +0,0 @@
-import DaoApp from "../../components/DaoApp";
-
-export default function Page() {
-  // server-rendered shell
-  return <DaoApp />;
-}

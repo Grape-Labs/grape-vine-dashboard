08a76d382239df40fd05fbe0d94855112e671b72
Kirk <kfournia@gmail.com>
2026-01-20 01:13:35 +0200

    updated logo

--- app/constants.tsx
+++ app/constants.tsx
@@ -28,5 +28,5 @@ export const tokens = [
 ]
 
 // constants.ts
-export const OG_LOGO = "/images/og_logo.png";
+export const OG_LOGO = "/images/og_logo_square.png";
 export const VINE_LOGO = './images/vine.jpg';
